db5d368d0d7c68c0e83b4a1cb42a7e1ca88401ca
Christopher Sardegna <gpg@reagentx.net>
2022-03-20 21:30:29 -0400

    Add alpha channel to Color

--- src/utilities/output/color.rs
+++ src/utilities/output/color.rs
@@ -1,13 +1,18 @@
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Color {
+    /// Red component
     pub r: u8,
+    /// Green component
     pub g: u8,
+    /// Blue component
     pub b: u8,
+    /// Alpha component
+    pub a: u8,
 }
 
 impl Color {
-    pub fn new(r: u8, g: u8, b: u8) -> Self {
-        Self { r, g, b }
+    pub fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
+        Self { r, g, b, a }
     }
 
     pub fn to_string(&self) -> String {
@@ -17,7 +22,12 @@ impl Color {
 
 impl Default for Color {
     fn default() -> Self {
-        Self { r: 0, g: 0, b: 0 }
+        Self {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: 255,
+        }
     }
 }
 
@@ -31,19 +41,21 @@ mod tests {
         assert_eq!(color.r, 0);
         assert_eq!(color.g, 0);
         assert_eq!(color.b, 0);
+        assert_eq!(color.a, 255);
     }
 
     #[test]
     fn can_create() {
-        let color = Color::new(1, 5, 50);
+        let color = Color::new(1, 5, 50, 100);
         assert_eq!(color.r, 1);
         assert_eq!(color.g, 5);
         assert_eq!(color.b, 50);
+        assert_eq!(color.a, 100);
     }
 
     #[test]
     fn can_get_string() {
-        let color = Color::new(1, 205, 50);
-        assert_eq!(color.to_string(), String::from("1 205 50"));
+        let color = Color::new(1, 205, 50, 255);
+        assert_eq!(color.to_string(), String::from("1 205 50\n"));
     }
 }
