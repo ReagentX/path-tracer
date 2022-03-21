687b7a9d98f51b472953e45b43533dd0eab96900
Christopher Sardegna <gpg@reagentx.net>
2022-03-20 21:17:22 -0400

    Simplify image drawing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod utilities;
 
-use crate::utilities::output::image::Image;
+use crate::utilities::output::{color::Color, image::Image};
 use std::time::Instant;
 
 fn main() {
@@ -9,18 +9,10 @@ fn main() {
 
     let now = Instant::now();
     for (idx, (row, col)) in Image::walk(image.width, image.height).enumerate() {
-        let r = (col as f64) / (image.height as f64);
-        let g = (row as f64) / (image.width as f64);
-        let b = 0.25;
-
-        let ir = (255.999 * r) as u8;
-        let ig = (255.999 * g) as u8;
-        let ib = (255.999 * b) as u8;
-
         let pixel = &mut image.buffer[idx];
-        pixel.r = ir;
-        pixel.g = ig;
-        pixel.b = ib;
+        pixel.r = (255.999 * (col as f64) / (image.height as f64)) as u8;
+        pixel.g = (255.999 * (row as f64) / (image.width as f64)) as u8;
+        pixel.b = (255.999 * 0.25) as u8;
     }
     println!("Rendered canvas in {}s", now.elapsed().as_secs());
 
