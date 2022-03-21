3cd6eb0e44f9f35d67a08d3392473fb4f258f21c
Christopher Sardegna <gpg@reagentx.net>
2022-03-20 23:38:11 -0400

    support multiprocessed rendering loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod utilities;
 
 use crate::utilities::{color::Color, image::Image};
+use rayon::prelude::*;
 use std::time::Instant;
 
 fn main() {
@@ -8,11 +9,22 @@ fn main() {
     let mut image = Image::from_ratio(600, 1.);
 
     let now = Instant::now();
-    for (idx, (row, col)) in Image::walk(image.width, image.height).enumerate() {
-        let pixel = &mut image.buffer[idx];
-        pixel.r = (255.999 * (col as f64) / (image.height as f64)) as u8;
-        pixel.g = (255.999 * (row as f64) / (image.width as f64)) as u8;
-        pixel.b = (255.999 * 0.25) as u8;
+    for row in (0..image.height).rev() {
+        let scanline: Vec<Color> = (0..image.width)
+            .into_par_iter()
+            .map(|col| {
+                Color::new(
+                    ((255 * col) / image.width) as u8,
+                    ((255 * row) / image.height) as u8,
+                    63_u8,
+                    255,
+                )
+            })
+            .collect();
+
+        for (col, pixel) in scanline.iter().enumerate() {
+            *image.color_at(row, col as u64) = *pixel;
+        }
     }
     println!("Rendered canvas in {}s", now.elapsed().as_secs());
 
