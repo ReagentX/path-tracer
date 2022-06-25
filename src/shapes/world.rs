d1761d58b174f0f66a13811244127adde315c64d
Christopher Sardegna <gpg@reagentx.net>
2022-06-25 11:33:16 -0400

    Fix y-axis bug in rendering engine

--- src/shapes/world.rs
+++ src/shapes/world.rs
@@ -6,10 +6,15 @@ impl Hittable for World {
     /// Iterate through each item in the world, returning the closest hit
     fn hit(&self, ray: &crate::utilities::ray::Ray, time_min: f64, time_max: f64) -> Option<Hit> {
         let mut hit: Option<Hit> = None;
+        // Stores the time it takes to hit the closest object to the camera
+        // This ensures that we respect the y-axis, that is, closer objects
+        // occlude farther objects
         let mut closest_so_far = time_max;
 
         self.iter().for_each(|shape| {
-            if let Some(contact) = shape.hit(ray, time_min, time_max) {
+            // Determine if the collision occurred
+            if let Some(contact) = shape.hit(ray, time_min, closest_so_far) {
+                // Update the time to hit the closest object
                 closest_so_far = contact.time;
                 hit = Some(contact)
             }
