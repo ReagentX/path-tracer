55d7addd442a95a3af78438a1fead383419d6668
Christopher Sardegna <gpg@reagentx.net>
2022-06-27 23:24:18 -0400

    Add random method

--- src/materials/scatter.rs
+++ src/materials/scatter.rs
@@ -8,4 +8,5 @@ pub type Material = Box<dyn Scatter>;
 pub trait Scatter: Send + Sync {
     fn scatter(&self, ray_in: &Ray, hit: &Hit) -> Option<(Color, Ray)>;
     fn emit(&self) -> Color;
+    fn random() -> Self where Self: Sized;
 }
