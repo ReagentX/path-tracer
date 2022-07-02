e2c0043bf40f4be50ee32b4c4e63011a2f6db542
Christopher Sardegna <gpg@reagentx.net>
2022-07-02 18:34:59 -0400

    Add time to ray

--- src/utilities/ray.rs
+++ src/utilities/ray.rs
@@ -3,11 +3,16 @@ use crate::utilities::point::Point;
 pub struct Ray {
     pub origin: Point,
     pub direction: Point,
+    pub time: f64,
 }
 
 impl Ray {
-    pub fn new(origin: Point, direction: Point) -> Self {
-        Ray { origin, direction }
+    pub fn new(origin: Point, direction: Point, time: f64) -> Self {
+        Ray {
+            origin,
+            direction,
+            time,
+        }
     }
 
     pub fn at(&self, time: f64) -> Point {
@@ -16,14 +21,14 @@ impl Ray {
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use crate::utilities::{point::Point, ray::Ray};
 
     #[test]
     fn can_create() {
         let p1 = Point::new(0., 0., 0.);
         let p2 = Point::new(5., 5., 5.);
-        let ray = Ray::new(p1, p2);
+        let ray = Ray::new(p1, p2, 0.);
         assert_eq!(ray.origin.x, 0.);
         assert_eq!(ray.origin.y, 0.);
         assert_eq!(ray.origin.z, 0.);
@@ -36,7 +41,7 @@ mod tests{
     fn can_get_at() {
         let p1 = Point::new(0., 0., 0.);
         let p2 = Point::new(5., 5., 5.);
-        let ray = Ray::new(p1, p2);
+        let ray = Ray::new(p1, p2, 0.);
         let ray_at_t1 = ray.at(0.5);
         assert_eq!(ray_at_t1.x, 2.5);
         assert_eq!(ray_at_t1.y, 2.5);
