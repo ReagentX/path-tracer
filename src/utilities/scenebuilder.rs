1345f2bd1d4cd32c67756a91434689f8c37f178e
Christopher Sardegna <github@reagentx.net>
2022-07-10 11:12:01 -0400

    Update scene

--- src/utilities/scenebuilder.rs
+++ src/utilities/scenebuilder.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use crate::{
     materials::{
         diffuse::Lambertian, glass::Dielectric, light::Light, metal::Metal, mirror::Mirror,
@@ -13,18 +15,18 @@ use super::{
         Orientation::{Landscape, Portrait},
     },
     point::Point,
-    scene::{Scene, Settings, RenderSettings},
+    scene::{RenderSettings, Scene, Settings},
 };
 
 pub fn build_scene() -> Scene {
     let render_settings = RenderSettings::new(10., 10, 1., 0., 1.);
-    let image = Image::widescreen(500, Landscape);
+    let image = Image::mobile(3, Portrait);
     // let image = Image::uhd(Landscape);
     // let camera = Camera::default_from_image(&image);
     let camera_settings = CameraSettings::new(
         Point::new(0., 1., 0.),
-        Point::new(-10., 0., 0.),
-        Point::new(10., -0.1, 0.),
+        Point::new(0., 0., 3.),
+        Point::new(0., 0., -2.),
         45.,
         image.aspect_ratio(),
         0.,
@@ -37,123 +39,24 @@ pub fn build_scene() -> Scene {
     let settings = Settings::new(render_settings, camera_settings);
 
     // Create world
-    let world: World = vec![
+    let mut world: World = vec![
         // Center
         Box::new(Sphere::new(
-            Point::new(0., 0., 0.),
-            Point::new(0., 0., 0.),
+            Point::new(0., -0.28, -6.),
+            Point::new(0., -0.28, -6.),
             settings.camera.shutter_open,
             settings.camera.shutter_close,
-            1.5,
-            Box::new(Mirror::new(Color::gray(0.9))),
+            1.4,
+            Box::new(Dielectric::new(Color::gray(0.9), 1.5)),
         )),
-        // Closest
+        // Light
         Box::new(Sphere::new(
-            Point::new(-3., -0.65, -3.2),
-            Point::new(-3., -0.65, -3.2),
+            Point::new(0., 10., -4.),
+            Point::new(0., 10., -4.),
             settings.camera.shutter_open,
             settings.camera.shutter_close,
-            1.,
-            Box::new(Metal::new(Color::gray(0.9), 0.9)),
-        )),
-        // Close
-        Box::new(Sphere::new(
-            Point::new(-1.7, -0.56, -1.7),
-            Point::new(-1.7, -0.56, -1.7),
-            settings.camera.shutter_open,
-            settings.camera.shutter_close,
-            1.,
-            Box::new(Dielectric::new(Color::rgb(0.6, 0.2, 0.9), 1.02)),
-        )),
-        // Far
-        Box::new(Sphere::new(
-            Point::new(3., -0.55, 2.5),
-            Point::new(3., -0.55, 2.5),
-            settings.camera.shutter_open,
-            settings.camera.shutter_close,
-            -1.,
-            Box::new(Lambertian::new(Color::gray(0.9), 1.)),
-        )),
-        // Farther
-        Box::new(Sphere::new(
-            Point::new(4.5, -0.7, 4.5),
-            Point::new(4.5, -0.7, 4.5),
-            settings.camera.shutter_open,
-            settings.camera.shutter_close,
-            1.,
-            Box::new(Mirror::new(Color::gray(0.8))),
-        )),
-        // Farthest
-        Box::new(Sphere::new(
-            Point::new(6.5, -1.1, 7.),
-            Point::new(6.5, -1.1, 7.),
-            settings.camera.shutter_open,
-            settings.camera.shutter_close,
-            1.,
-            Box::new(Metal::new(Color::rgb(0.1, 0.8, 0.7), 0.1)),
-        )),
-        // Farthest + 1
-        Box::new(Sphere::new(
-            Point::new(8.5, -1.45, 9.6),
-            Point::new(8.5, -1.45, 9.6),
-            settings.camera.shutter_open,
-            settings.camera.shutter_close,
-            1.,
-            Box::new(Lambertian::new(Color::gray(0.7), 1.)),
-        )),
-        // Farthest + 2
-        Box::new(Sphere::new(
-            Point::new(10.5, -1.85, 12.3),
-            Point::new(10.5, -1.85, 12.3),
-            settings.camera.shutter_open,
-            settings.camera.shutter_close,
-            1.,
-            Box::new(Metal::new(Color::rgb(0.8, 0.1, 0.7), 0.9)),
-        )),
-        // Front upper left
-        Box::new(Sphere::new(
-            Point::new(0., 2.1, 1.0),
-            Point::new(0., 2.1, 1.3),
-            settings.camera.shutter_open,
-            settings.camera.shutter_close,
-            0.2,
-            Box::new(Metal::new(Color::rgb(1., 0.7, 0.1), 0.3)),
-        )),
-        // Front upper right
-        Box::new(Sphere::new(
-            Point::new(0., 2.4, -1.6),
-            Point::new(0., 2.4, -1.4),
-            settings.camera.shutter_open,
-            settings.camera.shutter_close,
-            0.2,
-            Box::new(Metal::new(Color::rgb(0.7, 0.1, 1.), 0.7)),
-        )),
-        // Sun right
-        Box::new(Sphere::new(
-            Point::new(0., -1., 17.),
-            Point::new(0., -1., 17.),
-            settings.camera.shutter_open,
-            settings.camera.shutter_close,
-            7.,
-            Box::new(Light::new(Color::rgb(0.5, 0.5, 0.1), 8.)),
-        )),
-        // Sun left
-        Box::new(Sphere::new(
-            Point::new(0., -1., -17.),
-            Point::new(0., -1., -17.),
-            settings.camera.shutter_open,
-            settings.camera.shutter_close,
-            7.,
-            Box::new(Light::new(Color::rgb(0.5, 0.1, 0.5), 8.)),
-        )),
-        // Marble
-        Box::new(Sphere::new(
-            Point::new(0., -1.5, 3.5),
-            Point::new(0., -1.5, 3.5),
-            settings.camera.shutter_open,
-            settings.camera.shutter_close,
-            0.2,
-            Box::new(Dielectric::new(Color::gray(0.9), 1.4)),
+            5.,
+            Box::new(Light::new(Color::gray(1.), 4.)),
         )),
         // Ground
         Box::new(Sphere::new(
@@ -167,19 +70,19 @@ pub fn build_scene() -> Scene {
     ];
 
     // Add stars
-    // let mut rng = rand::thread_rng();
-    // for _ in 0..100 {
-    //     let random_x: f64 = rng.gen_range(-70f64..70f64);
-    //     let random_y: f64 = rng.gen_range(-15f64..35f64);
-    //     world.push(Box::new(Sphere::new(
-    //         Point::new(random_x, random_y, -100.),
-    //         Point::new(random_x, random_y, -100.),
-    //         settings.camera.shutter_open,
-    //         settings.camera.shutter_close,
-    //         0.3,
-    //         Box::new(Light::new(Color::random(), 10.)),
-    //     )));
-    // }
+    let mut rng = rand::thread_rng();
+    for _ in 0..100 {
+        let random_x: f64 = rng.gen_range(-55f64..55f64);
+        let random_y: f64 = rng.gen_range(-20f64..75f64);
+        world.push(Box::new(Sphere::new(
+            Point::new(random_x, random_y, -100. + (random_x.abs() / 2.)),
+            Point::new(random_x, random_y, -100. + (random_x.abs() / 2.)),
+            settings.camera.shutter_open,
+            settings.camera.shutter_close,
+            0.3,
+            Box::new(Light::new(Color::random(), 5.)),
+        )));
+    }
     // for _ in 0..50 {
     //     let mut rng = rand::thread_rng();
     //     let random_x: f64 = rng.gen_range(-300f64..300f64);
